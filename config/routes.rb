42fef64703d0249e6844c2283d1a32c9f82bc371
monkeylizard <Gazebo@Amadeus-4.local>
2014-01-01 22:49:14 -0500

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ThinkHappy::Application.routes.draw do
-  get "thoughts/new"
+  root 'thoughts#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
